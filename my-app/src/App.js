adf3e63f195d1135d534b2c12503384412d758ca
Dhaneesh Thulasidharan <Dhaneesh.Thulasdharan@refinitiv.com>
2020-08-07 11:18:11 +0530

    removed home-task 1 form  master

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { CreateReactComponent } from "./home-task-1/create-react-component";
-import { CreateReactPureComponent } from "./home-task-1/create-pure-react-component";
-
 
 function App() {
   return (
@@ -11,9 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello from Function Component</p>
-        <p><CreateReactComponent /></p>
-        <p><CreateReactPureComponent /></p>
-
       </header>
     </div>
   );
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-import "./home-task-1/create-element";
-
 ReactDOM.render(
   <React.StrictMode>
     <App /> 
